65d30ed0c13ace083ddf04355460833305ae2dec
Eric Mervin <ericmervin007@gmail.com>
2021-08-02 00:09:44 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const hamburger = document.getElementById('hamburger');
+const navUL = document.getElementById('nav_links');
+
+hamburger.onmouseover('click', () => {
+    alert("Hello! I am an alert box!!");
+})
